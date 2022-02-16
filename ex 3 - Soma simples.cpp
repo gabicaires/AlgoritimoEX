59f267485fc9d2e741bc281f38bab9895841e6fe
Gabriela Caires <99662184+gabicaires@users.noreply.github.com>
2022-02-16 16:48:41 -0300

    Exercício 3

---
+++ ex 3 - Soma simples.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+int a, b, soma;
+printf("Digite o primeiro numero: ");
+scanf("%d", &a);
+printf("Digite o segunrdo numero: ");
+scanf("%d", &b);
+
+soma = a + b;
+
+printf("soma = %d", soma);
+}
